c41fd7c94e3d5450819e9370b2319b464052a8ef
Hamza Harsal <hamzaharsal10@gmail.com>
2025-01-15 11:19:13 +0100

    koppeling

---
+++ src/main/java/com/example/co2mpare/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.co2mpare;
+
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    public void goToHome() {
+        System.out.println("Navigeren naar het Home-scherm.");
+        // Voeg hier navigatielogica toe
+    }
+
+    @FXML
+    public void goToStats() {
+        System.out.println("Navigeren naar het Statistieken-scherm.");
+        // Voeg hier navigatielogica toe
+    }
+
+    @FXML
+    public void goToSettings() {
+        System.out.println("Navigeren naar het Instellingen-scherm.");
+        // Voeg hier navigatielogica toe
+    }
+}
